5881ab21a9437ef2f6f3f91049453a4748438727
Lucas Aranchipe <aranchipe98@gmail.com>
2024-12-20 01:09:57 -0300

    Deploy

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,24 @@
-import type { NextConfig } from 'next';
+import { NextConfig } from 'next';
 
+/** @type {NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
-  images: {
-    remotePatterns: [
+  async headers() {
+    return [
       {
-        protocol: 'https',
-        hostname: 's2.glbimg.com',
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' }, // replace this with your actual origin
+          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
       },
-      {
-        protocol: 'https',
-        hostname: 'static.ifood-static.com.br',
-      },
-    ],
+    ];
   },
 };
 
