6773186570b61195b0e53e179da8c3bcc35ed185
Lucas Aranchipe <aranchipe98@gmail.com>
2024-12-20 02:48:49 -0300

    Project Done

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,7 @@ import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
 export function middleware(req: NextRequest) {
+  console.log(req);
   const response = NextResponse.next();
 
   // Configurar os headers de CORS
