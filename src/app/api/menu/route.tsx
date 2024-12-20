9552c2e9ec0c0aca457da2166c6fcd0153d0d398
Lucas Aranchipe <aranchipe98@gmail.com>
2024-12-20 03:28:58 -0300

    Project Done

--- src/app/api/menu/route.tsx
+++ src/app/api/menu/route.tsx
@@ -13,3 +13,13 @@ export async function GET() {
     return NextResponse.json({ error: 'Error fetching data' }, { status: 500 });
   }
 }
+
+// Lidar com o método OPTIONS
+export async function OPTIONS() {
+  const headers = new Headers();
+  headers.set('Access-Control-Allow-Origin', '*');
+  headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  return new Response(null, { headers });
+}
